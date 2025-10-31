e319a46ca31a95027bd88fb3b219538a8b5369c9
0202karuma-afk <0202karuma@gmail.com>
2025-10-31 15:11:05 +0900

    ギミック3の実装
    ギミック3とギミック3によって壊れる壁を実装

---
+++ Assets/01_GameData/Scripts/FruitCollision.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class FruitCollision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("BreakableWall"))
+        {
+            Destroy(collision.gameObject); 
+            Destroy(this.gameObject);      
+        }
+    }
+}
