7a5e6ad643cbe44e96f0871e0c8397da66e57858
MikamiKanta <kss-25210294@nsgcl.jp>
2025-11-03 12:57:38 +0900

    摩擦の調整
    ・壁にくっつかなくなった
    ・ついでにゲームオーバー時にスタート地点から始まるようにした

--- Assets/01_GameData/Scripts/PlayerController.cs
+++ Assets/01_GameData/Scripts/PlayerController.cs
@@ -3,15 +3,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rbody;
+    Collider2D col;
+
     float axisH = 0.0f;
     public float Speed = 3.0f;
     public float JumpPw = 9.0f;
     public LayerMask groundLayer;
     bool goJump = false;
 
+    [Header("Physics Materials")]
+    [SerializeField] PhysicsMaterial2D normalMat;     
+    [SerializeField] PhysicsMaterial2D noFrictionMat; 
+
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
     }
 
     void Update()
@@ -47,7 +54,7 @@ public class PlayerController : MonoBehaviour
 
         if (onGround && goJump)
         {
-            rbody.AddForce(Vector2.up * JumpPw, ForceMode2D.Impulse);//transformÇ≈í≤êÆ
+            rbody.AddForce(Vector2.up * JumpPw, ForceMode2D.Impulse);
             goJump = false;
         }
     }
@@ -55,5 +62,28 @@ public class PlayerController : MonoBehaviour
     public void Jump()
     {
         goJump = true;
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        foreach (var contact in collision.contacts)
+        {
+            if (Vector2.Angle(contact.normal, Vector2.up) < 30f)
+            {
+                col.sharedMaterial = normalMat;
+                return;
+            }
+            else if (Mathf.Abs(Vector2.Angle(contact.normal, Vector2.left)) < 30f ||
+                     Mathf.Abs(Vector2.Angle(contact.normal, Vector2.right)) < 30f)
+            {
+                col.sharedMaterial = noFrictionMat;
+                return;
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        col.sharedMaterial = normalMat;
     }
 }
