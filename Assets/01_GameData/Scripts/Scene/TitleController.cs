8ec7cb42892f2e6a5e91865f81c95b4dc09ad60f
MikamiKanta <kss-25210294@nsgcl.jp>
2025-11-21 19:12:46 +0900

    BGMの追加
    タイトルにBGMを追加。

--- Assets/01_GameData/Scripts/Scene/TitleController.cs
+++ Assets/01_GameData/Scripts/Scene/TitleController.cs
@@ -7,25 +7,50 @@ public class TitleController : MonoBehaviour
 {
     private PlayerInput playerInput;
     private InputAction submitAction;
+    private InputAction toggleBgmAction;
+    private AudioSource audioSource;
 
     void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
         submitAction = playerInput.actions["Submit"];
+        toggleBgmAction = playerInput.actions["ToggleBGM"];
+        audioSource = GetComponent<AudioSource>();
     }
 
     void OnEnable()
     {
         submitAction.performed += OnSubmit;
+        if (toggleBgmAction != null)
+            toggleBgmAction.performed += OnToggleBgm;
     }
 
     void OnDisable()
     {
         submitAction.performed -= OnSubmit;
+        if (toggleBgmAction != null)
+            toggleBgmAction.performed -= OnToggleBgm;
     }
     private void OnSubmit(InputAction.CallbackContext context)
     {
         // Spaceキー or コントローラーのSouthボタンでMainSceneへ
         SceneManager.LoadScene("MainGame");
     }
+    private void OnToggleBgm(InputAction.CallbackContext context)
+    {
+        // AudioSource が存在しなければ何もしない
+        if (audioSource == null)
+            return;
+
+        // 再生中なら停止、停止中なら再生
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+        else
+        {
+            audioSource.Play();
+        }
+    }
+
 }
