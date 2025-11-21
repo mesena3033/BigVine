0fa0b0e7ea103c23289c9da3253405550ad61d16
MikamiKanta <kss-25210294@nsgcl.jp>
2025-11-21 18:25:13 +0900

    シーンの追加
    Title,GameClearシーンを追加。
    デフォルトはTitleからスタート。
    Title,GameOverともに、"South"ボタンで遷移→"East"ボタンで遷移に変更。

---
+++ Assets/01_GameData/Scripts/Scene/TitleController.cs
@@ -0,0 +1,31 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private InputAction submitAction;
+
+    void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        submitAction = playerInput.actions["Submit"];
+    }
+
+    void OnEnable()
+    {
+        submitAction.performed += OnSubmit;
+    }
+
+    void OnDisable()
+    {
+        submitAction.performed -= OnSubmit;
+    }
+    private void OnSubmit(InputAction.CallbackContext context)
+    {
+        // Spaceキー or コントローラーのSouthボタンでMainSceneへ
+        SceneManager.LoadScene("MainGame");
+    }
+}
