88a3b240888bad08c4a06b702b9e8c5487574584
MikamiKanta <kss-25210294@nsgcl.jp>
2025-11-22 22:31:50 +0900

    クリアでシーン遷移を実装
    ・クリアでクリアシーンに遷移
    ・クリアBGM(仮)の実装
    ・ゲームオーバーBGM(仮)の実装

---
+++ Assets/01_GameData/Scripts/Scene/ClearTrigger.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // シーン遷移に必要
+
+public class ClearTrigger : MonoBehaviour
+{
+    // 遷移先のシーン名（Inspectorから変更可能）
+    [SerializeField] private string nextSceneName = "GameClear";
+
+    // 2Dの当たり判定（Is TriggerがONのもの）に入った時に呼ばれる
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 触れたのが「Player」というタグがついたオブジェクトなら
+        if (collision.CompareTag("Player"))
+        {
+            // シーンをロードする
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
